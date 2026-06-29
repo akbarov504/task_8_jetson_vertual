d0bcd8ae42f1ed00ef034a2d37fc626597568666
Akbar Akbarov <akbarovakbar888@gmail.com>
2026-06-29 11:45:29 +0500

    change video ports

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from datetime import datetime, timedelta, timezone
 from config import LOCAL_PATH, VIDEO_SEGMENT_LEN
 from db import init_db, insert_video, video_exists
 
-OUT_VIDEO_DEVICE = "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.1:1.0-video-index0"
-OUT_AUDIO_DEVICE = "hw:Camera_1,0"
+OUT_VIDEO_DEVICE = "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.2:1.0-video-index0"
+OUT_AUDIO_DEVICE = "hw:Camera,0"
 
-IN_VIDEO_DEVICE = "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.2:1.0-video-index0"
-IN_AUDIO_DEVICE = "hw:Camera,0"
+IN_VIDEO_DEVICE = "/dev/v4l/by-path/platform-3610000.usb-usb-0:2.1:1.0-video-index0"
+IN_AUDIO_DEVICE = "hw:Camera_1,0"
 
 OUT_VIRTUAL_VIDEO_DEVICE = "/dev/video40"
 IN_VIRTUAL_VIDEO_DEVICE  = "/dev/video41"
